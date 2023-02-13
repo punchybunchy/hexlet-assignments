0b7ae2649cdefeecb35380bc37719a6d278a2a0f
@hexlet/cli <support@hexlet.io>
2023-02-13 18:30:46 +0600

    submit java-spring-ru/spring-orm

---
+++ java-spring-ru/spring-orm/src/main/java/exercise/controller/PeopleController.java
@@ -0,0 +1,57 @@
+package exercise.controller;
+
+import exercise.model.Person;
+import exercise.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/people")
+public class PeopleController {
+
+    // Автоматически заполняем значение поля
+    @Autowired
+    private PersonRepository personRepository;
+
+    @GetMapping(path = "/{id}")
+    public Person getPerson(@PathVariable long id) {
+        return this.personRepository.findById(id);
+    }
+
+    @GetMapping(path = "")
+    public Iterable<Person> getPeople() {
+        return this.personRepository.findAll();
+    }
+
+    // BEGIN
+    @PostMapping(path = "")
+    public void createPerson(@RequestBody String firstName, String lastName) {
+        Person person = new Person();
+        person.setFirstName(firstName);
+        person.setLastName(lastName);
+        this.personRepository.save(person);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void deletePerson(@PathVariable Long id) {
+        this.personRepository.deleteById(id);
+    }
+
+    @PatchMapping(path = "/{id}")
+    public void updatePerson(@PathVariable Long id, @RequestBody String firstName, String lastName) {
+        Person person = getPerson(id);
+        person.setFirstName(firstName);
+        person.setLastName(lastName);
+        this.personRepository.save(person);
+    }
+    // END
+}
